b4cf4b4ef5cfd89686da5668f019eff8cd357cd4
Resu <Resu@DESKTOP-UAMN0M3>
2020-12-10 13:43:47 +0300

    Finaly it works!!!!

--- Lab_1_2_3/Program.cs
+++ Lab_1_2_3/Program.cs
@@ -13,9 +13,13 @@ namespace Lab_1_2_3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            await Task.Run(() => Application.Run(new Lab_1()));
-            await Task.Run(() => Application.Run(new Lab_2()));
-            await Task.Run(() => Application.Run(new Lab_3()));
+            //await Task.Run(() => Application.Run(new Lab_1()));
+            //await Task.Run(() => Application.Run(new Lab_2()));
+            //await Task.Run(() => Application.Run(new Lab_3()));
+            Task t1 = Task.Run(() => Application.Run(new Lab_1()));
+            Task t2 = Task.Run(() => Application.Run(new Lab_2()));
+            Task t3 = Task.Run(() => Application.Run(new Lab_3()));
+            await Task.WhenAll(new[] { t1, t2, t3 });
         }
     }
 }
